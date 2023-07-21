1b6956b1ce4b3a9233f441a844bcb385fcb120af
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-07-22 01:58:06 +0800

    fix(bug): fix wrong progress used

--- bricks/clean_architecture_skeleton/hooks/pre_gen.dart
+++ bricks/clean_architecture_skeleton/hooks/pre_gen.dart
@@ -42,7 +42,7 @@ void run(HookContext context) async {
     );
   }
 
-  var process23 = context.logger.progress('Check if .env is present!');
+  var process3 = context.logger.progress('Check if .env is present!');
   result = await Process.run(
     'test',
     [
@@ -52,10 +52,10 @@ void run(HookContext context) async {
   );
 
   if (result.exitCode != 0) {
-    process2.fail(
+    process3.fail(
       '.env not present!',
     );
-    var process3 = context.logger.progress('Copying .env.example to .env');
+    var process4 = context.logger.progress('Copying .env.example to .env');
     result = await Process.run(
       'cp',
       [
@@ -65,17 +65,17 @@ void run(HookContext context) async {
     );
 
     if (result.exitCode != 0) {
-      process3.fail(
+      process4.fail(
         'Failed crete .env file. Make sure you have .env or .env.example in root directory before continuing the process!',
       );
       exit(1);
     } else {
-      process3.complete(
+      process4.complete(
         '.env generated!',
       );
     }
   } else {
-    process2.complete(
+    process3.complete(
       '.env file exists!',
     );
   }
