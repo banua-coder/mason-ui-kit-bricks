329d693d13de32ab102a593d475811e769cad14e
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-07-23 15:09:53 +0800

    feat(hooks): add hooks to delete unused file

--- bricks/ui_kit_core/hooks/pre_gen.dart
+++ bricks/ui_kit_core/hooks/pre_gen.dart
@@ -1,3 +1,36 @@
-import 'package:mason/mason.dart';
+import 'dart:io';
 
-void run(HookContext context) {}
+import 'package:mason/mason.dart';
+import 'package:recase/recase.dart';
+
+void run(HookContext context) async {
+  var appName = ReCase(context.vars['name']);
+
+  var checkTestProgress = context.logger.progress('Check for unused file!');
+  var result = await Process.run('test', [
+    '-e',
+    'test/${appName.snakeCase}_ui_kit_test.dart',
+  ]);
+
+  if (result.exitCode == 0) {
+    checkTestProgress.complete('Some unused file detected!');
+
+    var removeUnusedFileProgress =
+        context.logger.progress('Removing all unused file!');
+    result = await Process.run(
+      'rm',
+      [
+        'test/${appName.snakeCase}_ui_kit_test.dart',
+      ],
+    );
+
+    if (result.exitCode == 0) {
+      removeUnusedFileProgress
+          .complete('All unused file successfully removed!');
+    } else {
+      removeUnusedFileProgress.fail('Failed to remove some unused file!');
+    }
+  } else {
+    checkTestProgress.complete('There are no unused file detected!');
+  }
+}
