994969ef4a19e8b5918759a3fe9d99adc74a6f85
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-07-28 19:19:18 +0800

    refactor(serializer): rename bool serializer to int to bool serializer

--- bricks/clean_architecture_skeleton/__brick__/lib/utils/serializers/int_to_bool_serializer.dart
+++ bricks/clean_architecture_skeleton/__brick__/lib/utils/serializers/int_to_bool_serializer.dart
@@ -1,7 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 
-class BoolSerializer extends JsonConverter<bool, int> {
-  const BoolSerializer();
+class IntToBoolSerializer extends JsonConverter<bool, int> {
+  const IntToBoolSerializer();
 
   @override
   bool fromJson(int json) => json == 1;
