2587f81e476a459282fc0c8dea5073f96f39a8aa
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-11-16 18:14:07 +0800

    refactor(ui_kit_component): remove unused file

--- bricks/ui_kit_component/__brick__/lib/{{packageName}}.dart
+++
@@ -1,3 +0,0 @@
-
-{{#hasEnum}}export '{{{componentPath}}}/enums/{{prefix.snakeCase()}}_{{name.snakeCase()}}_enums.dart';{{/hasEnum}}
-export '{{{componentPath}}}/{{prefix.snakeCase()}}_{{name.snakeCase()}}.dart';
